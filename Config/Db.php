213995145fb39eb6fe9a610a1275e27873075dba
Prince Radadiya <princeradadiya@example.com>
2026-03-07 11:56:09 +0530

    db

--- Config/Db.php
+++ Config/Db.php
@@ -13,7 +13,8 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 try {
 
-    $client = new MongoDB\Client("mongodb://localhost:27017");
+    // $client = new MongoDB\Client("mongodb://localhost:27017");
+    $client = new MongoDB\Client("mongodb+srv://statusall08:prince123A@cluster0.cjm5jcg.mongodb.net/AriPen");
 
     $db = $client->AriPen;
 
